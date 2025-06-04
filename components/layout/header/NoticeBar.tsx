56ee50d4dcf946297af6202751008247f8f5d1c0
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-05 04:11:31 +0600

    added

--- components/layout/header/NoticeBar.tsx
+++ components/layout/header/NoticeBar.tsx
@@ -2,7 +2,7 @@ export default function NoticeBar() {
   return (
     <div className="w-full bg-yellow-100 border-b border-yellow-300 text-yellow-900 text-sm text-center py-2 px-4 sticky top-0 z-50">
       🚧 Our shop is still in development. You may experience limited
-      functionality.
+      functionality. Thank you
     </div>
   );
 }
