7d82221efc27471f540ea954eed02ddc18124a47
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-06 11:03:18 +0600

    tag manager Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,20 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-52BGCSCX');
+    `,
+          }}
+        />
+
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-0B8QYN6WLM"
           strategy="afterInteractive"
         />
@@ -78,6 +92,15 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${lato.variable} antialiased `}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-52BGCSCX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <ReduxProvider>
           <FaviconSwitcher />
           <ThemeWrapper>
