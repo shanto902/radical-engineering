15d37ab06f5caaa5dc819755feda99483c269c82
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-18 00:12:02 +0600

    added pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
@@ -143,6 +145,24 @@ export default async function RootLayout({
             `,
           }}
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1255308732866949');
+      fbq('track', 'PageView');
+    `,
+          }}
+        />
       </head>
 
       <body className={`${lato.variable} antialiased`}>
@@ -154,6 +174,14 @@ export default async function RootLayout({
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
+        </noscript>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1255308732866949&ev=PageView&noscript=1"
+          />
         </noscript>
 
         <ReduxProvider>
