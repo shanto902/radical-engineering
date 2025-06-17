df0b1d78d095a456b5f95136fa74330bb1c12304
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-17 17:58:19 +0600

    added styles

--- app/cookies-policy/page.tsx
+++ app/cookies-policy/page.tsx
@@ -1,10 +1,32 @@
 import PaddingContainer from "@/components/common/PaddingContainer";
+import { Metadata } from "next";
 import React from "react";
-
+export const metadata: Metadata = {
+  title: "Cookie Policy | Radical Engineering",
+  description: "Cookie Policy page of Radical Engineering",
+  openGraph: {
+    title: "Cookie Policy | Radical Engineering",
+    description: "Cookie Policy page of Radical Engineering",
+    images: [
+      {
+        url: "/og/cookie-policy.jpg", // Ensure this path is public (inside the `public` directory)
+        width: 1200,
+        height: 630,
+        alt: "Cookie Policy Cover - Radical Engineering",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Cookie Policy | Radical Engineering",
+    description: "Cookie Policy page of Radical Engineering",
+    images: ["/og/cookie-policy.jpg"],
+  },
+};
 const page = () => {
   return (
-    <PaddingContainer>
-      <h1>Cookie Policy</h1>
+    <PaddingContainer className="rich-text my-5">
+      <h2>Cookie Policy</h2>
       <p>
         <strong>Last updated:</strong> June 17, 2025
       </p>
--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,11 +1,33 @@
 import PaddingContainer from "@/components/common/PaddingContainer";
+import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
-
+export const metadata: Metadata = {
+  title: "Privacy Policy | Radical Engineering",
+  description: "Privacy Policy page of Radical Engineering",
+  openGraph: {
+    title: "Privacy Policy | Radical Engineering",
+    description: "Privacy Policy page of Radical Engineering",
+    images: [
+      {
+        url: "/og/privacy-policy.jpg", // Ensure this path is public (inside the `public` directory)
+        width: 1200,
+        height: 630,
+        alt: "Privacy Policy Cover - Radical Engineering",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Privacy Policy | Radical Engineering",
+    description: "Privacy Policy page of Radical Engineering",
+    images: ["/og/privacy-policy.jpg"],
+  },
+};
 const page = () => {
   return (
-    <PaddingContainer>
-      <h1>Privacy Policy</h1>
+    <PaddingContainer className="rich-text my-5">
+      <h2>Privacy Policy</h2>
       <p>
         <strong>Last updated:</strong> June 17, 2025
       </p>
