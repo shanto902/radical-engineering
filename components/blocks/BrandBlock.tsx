1de9679be79f0422a7df977536fadb6286120f14
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-19 00:12:27 +0600

    added

--- components/blocks/BrandBlock.tsx
+++ components/blocks/BrandBlock.tsx
@@ -4,7 +4,7 @@ import BrandMarquee from "../BrandMarquee";
 const BrandBlock = async () => {
   const brands = await fetchBrands();
   return (
-    <section className="bg-background py-12 px-4 sm:px-6 lg:px-8">
+    <section className="bg-backgroundLight dark:bg-backgroundDark py-5 md:py-12 px-4 sm:px-6 lg:px-8">
       {/* <PaddingContainer>
         <h2 className="text-2xl text-center md:text-3xl font-bold mb-8">
           {header_text || "Featured Categories"}
