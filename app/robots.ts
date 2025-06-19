5fe51f2c943e9bac11c243cbe6d75c5f14bfe124
Ashik Ali Shanto <shanto902@gmail.com>
2025-06-19 23:35:38 +0600

    added google feed route v2

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,20 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot-Image",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}sitemap.xml`,
   };
 }
